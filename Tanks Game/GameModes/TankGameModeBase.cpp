86360cceca347fb0ee14bd65c70af03145817ee6
Diel01 <71090440+Diel01@users.noreply.github.com>
2020-10-04 20:33:05 -0300

    Add files via upload

---
+++ Tanks Game/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,73 @@
+// Copyright Nicolas Diel
+
+
+#include "TankGameModeBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "ToonTanks/Pawns/PawnTank.h"
+#include "ToonTanks/Pawns/PawnTurret.h"
+#include "ToonTanks/PlayersControllers/PlayerControllerBase.h"
+
+
+void ATankGameModeBase::BeginPlay() 
+{
+    Super::BeginPlay();
+
+    HandleGameStart();
+}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor) 
+{
+    // Check what type of actor died, If Turrent, tally. If Player -> go to lose condition
+    if (DeadActor == PlayerTank)
+    {
+        PlayerTank->HandleDestruction();
+        HandleGameOver(false);
+
+        if (PlayerControllerRef)
+        {
+            PlayerControllerRef->SetPlayerEnabledState(false);
+        }
+    }
+    else if (APawnTurret* DestroyedTurret = Cast<APawnTurret>(DeadActor))
+    {
+        DestroyedTurret->HandleDestruction();
+
+        if(--TargetTurrets == 0)
+        {
+            HandleGameOver(true);
+        }
+    }
+}
+
+void ATankGameModeBase::HandleGameStart() 
+{
+    TargetTurrets = GetTargetTurretCount();
+    PlayerTank = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+    PlayerControllerRef = Cast<APlayerControllerBase>(UGameplayStatics::GetPlayerController(this, 0));
+
+    // Initialise the start countdown, turret activation, paw check,et.
+    GameStart();
+
+    if (PlayerControllerRef)
+    {
+        PlayerControllerRef->SetPlayerEnabledState(false);
+
+        FTimerHandle PlayerEnableHandle;
+        FTimerDelegate PlayerEnableDelegate = FTimerDelegate::CreateUObject(PlayerControllerRef, &APlayerControllerBase::SetPlayerEnabledState, true);
+        GetWorld()->GetTimerManager().SetTimer(PlayerEnableHandle, PlayerEnableDelegate, StartDelay, false);
+    }
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon) 
+{
+    // See if the player has destroyed all the turrets, show win result,
+    //else if turret destroyed player, show lose result
+    GameOver(PlayerWon);
+}
+
+int32 ATankGameModeBase::GetTargetTurretCount() 
+{
+    TArray<AActor*> TurretActors;
+    UGameplayStatics::GetAllActorsOfClass(GetWorld(), APawnTurret::StaticClass(), TurretActors);
+    return TurretActors.Num();    
+}
