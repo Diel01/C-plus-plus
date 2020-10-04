cafdf89654391f41b8d56c19dc8899da21673255
Diel01 <71090440+Diel01@users.noreply.github.com>
2020-10-04 20:33:35 -0300

    Add files via upload

---
+++ Tanks Game/Pawns/PawnTurret.cpp
@@ -0,0 +1,59 @@
+// Copyright Nicolas Diel
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+#include "PawnTank.h"
+
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+    GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+
+    PlayerPawn = Cast<APawnTank>(UGameplayStatics::GetPlayerPawn(this, 0));
+}
+
+void APawnTurret::HandleDestruction() 
+{
+    // Call base pawn class HandleDestruction to play effects
+    Super::HandleDestruction();
+    Destroy();
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    if(!PlayerPawn || ReturnDistanceToPlayer() > FireRange)
+    {
+        return;
+    }
+
+    RotateTurret(PlayerPawn->GetActorLocation());
+}
+
+void APawnTurret::CheckFireCondition() 
+{
+    if (!PlayerPawn || !PlayerPawn->GetIsPlayerAlive())
+    {
+        return;
+    }
+    
+    if (ReturnDistanceToPlayer() <= FireRange)
+    {
+        Fire();
+    }
+}
+
+float APawnTurret::ReturnDistanceToPlayer() 
+{
+    if (!PlayerPawn)
+    {
+        return 0.f;
+    }
+
+    return FVector::Dist(PlayerPawn->GetActorLocation(), GetActorLocation());
+}
