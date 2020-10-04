059307dc917cb741252e6d321a100704a5a6831d
Diel01 <71090440+Diel01@users.noreply.github.com>
2020-10-04 20:34:00 -0300

    Add files via upload

---
+++ Tanks Game/PlayersControllers/PlayerControllerBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Nicolas Diel
+
+
+#include "PlayerControllerBase.h"
+
+
+void APlayerControllerBase::SetPlayerEnabledState(bool SetPlayerEnabled) 
+{
+    if (SetPlayerEnabled)
+    {
+        GetPawn()->EnableInput(this);
+    }
+    else
+    {
+        GetPawn()->DisableInput(this);
+    }
+    bShowMouseCursor = SetPlayerEnabled;
+}
